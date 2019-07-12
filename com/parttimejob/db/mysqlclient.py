6dfd0276f29abb00647b0296650cfe0d75ec7b09
cosmo zhu <cosmo.zhu@scai-global.com>
2019-07-12 16:28:37 +0800

    修改用户名密码

--- com/parttimejob/db/mysqlclient.py
+++ com/parttimejob/db/mysqlclient.py
@@ -13,7 +13,7 @@ import time
 class MysqlClient(object):
 
     def __init__(self):
-        self.db = pymysql.connect("127.0.0.1", "ride_master", "ride_master", "less_credit")
+        self.db = pymysql.connect("127.0.0.1", "cosmo", "cosmo123456", "cosmo")
         # 使用cursor()方法获取操作游标
         self.cursor = self.db.cursor()
 
