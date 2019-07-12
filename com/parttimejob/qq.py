204649812f6a7b43657a23a622c34827b1193459
cosmo zhu <cosmo.zhu@scai-global.com>
2019-07-12 19:13:58 +0800

    修改为本地地址

--- com/parttimejob/qq.py
+++ com/parttimejob/qq.py
@@ -11,6 +11,5 @@ def sendText(group_id):
     response = requests.get("http://127.0.0.1:5700/send_group_msg?group_id=" + group_id + "&message=世界你好 \n "
                                                                                           "hellow world！")
 
-
 if __name__ == '__main__':
     sendText('827718520')
