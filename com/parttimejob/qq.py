253633ff3627a837373411c93523d213398a5685
cosmo zhu <cosmo.zhu@scai-global.com>
2019-07-14 10:24:25 +0800

    在调用cq http api 时对中文内容进行encode

--- com/parttimejob/qq.py
+++ com/parttimejob/qq.py
@@ -1,15 +1,22 @@
 # coding:UTF-8
+import urllib
 
 import requests
 
 
 def sendMes(group_id, msg):
-    response = requests.get("http://127.0.0.1:5700/send_group_msg?group_id=" + group_id + "&message=" + msg)
+    msg = urllib.parse.quote(msg)
+
+    url = "http://127.0.0.1:5700/send_group_msg?group_id=" + group_id + "&message=" +msg
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("消息推送失败")
 
 
 def sendText(group_id):
     response = requests.get("http://127.0.0.1:5700/send_group_msg?group_id=" + group_id + "&message=世界你好 \n "
                                                                                           "hellow world！")
 
+
 if __name__ == '__main__':
-    sendText('827718520')
+    sendMes('827718520','你好啊')
