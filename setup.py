d4c8b144d3eddba3435e64e6999eb0406dcc0988
cosmozhu <zhuchao1103@gmail.com>
2020-01-19 14:11:47 +0800

    增加自定义包打包函数

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 
-setup(name='MysqlClient', version='1.0', description='define MysqlClient', autor='cosmozhu',
+setup(name='com.parttimejob.db.mysqlclient', version='1.0', description='define MysqlClient', autor='cosmozhu',
       py_modules=['com.parttimejob.db.mysqlclient'])
